b18ab5687329de8c5300ac32be43ef5a27b87cf1
Uncle Hunto <unclehunto@yahoo.com>
2016-11-01 13:30:59 -0700

    Add repo scripts

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in */
+do
+	cd "$i"
+	updpkgsums
+	du ./*.pkg.tar.xz > namcap-pkg.log
+	namcap ./*.pkg.tar.xz >> namcap-pkg.log
+	namcap PKGBUILD >> namcap-pkgbuild.log
+	cd -
+done
